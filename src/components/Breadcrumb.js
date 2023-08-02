634cd6d783bc11a2b60c3de421af18b9fc20457f
Lim_JiSeon <dpf226@naver.com>
2023-08-02 13:00:46 +0900

    [FEAT] Breadcrumb 로직 에러 해결 중

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -1,41 +1,46 @@
+import { getData } from "../utils/api.js";
+
 export default function Breadcrumb({ $target, postId }) {
   const $breadcrumb = document.createElement("div");
   $breadcrumb.className = "breadcrumb";
+  const breadcrumb = {};
 
   this.state = postId;
 
   $target.appendChild($breadcrumb);
 
-  this.setState = (nextState) => {
+  this.setState = async (nextState) => {
     this.state = nextState;
+    const documents = await getData("/documents");
+    this.makeBreadcrumb(false, documents);
+    console.log(breadcrumb);
     this.render();
   };
 
   this.render = () => {
-    $breadcrumb.innerHTML = `
-        
-    `;
+    const targetDocument = breadcrumb[this.state];
+    targetDocument.forEach((item) => {
+      $documentLink = document.createElement("div");
+      $documentLink.textContent = item[0];
+      $breadcrumb.appendChild($documentLink);
+    });
   };
 
-  this.makeBreadcrumb = async (postId) => {
-    const documents = await getData("/documents");
-    let breadcrumb = {}
-    for (let i = 0; )
-  }
+  this.makeBreadcrumb = async (targetItem, data) => {
+    data.forEach(({ title, documents, id }) => {
+      targetItem
+        ? targetItem.push([title, id])
+        : (breadcrumb[id] = [[title, id]]);
+
+      if (documents.length > 0) {
+        this.makeBreadcrumb(breadcrumb[id], documents);
+      } else {
+        this.makeBreadcrumb(false, documents);
+      }
+    });
+  };
+
+  $breadcrumb.addEventListener("keyup", (e) => {
+    const { target } = e;
+  });
 }
-this.render();
-
-$breadcrumb.addEventListener("keyup", (e) => {
-  const { target } = e;
-
-  const name = target.getAttribute("name");
-
-  if (this.state[name] !== undefined) {
-    const nextState = {
-      ...this.state,
-      [name]: target.value,
-    };
-    this.setState(nextState);
-    onEditing(this.state);
-  }
-});
